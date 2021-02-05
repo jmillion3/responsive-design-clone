7544a9d9eb12c6edb02b2f0a7aa7b9ee95905f5a
Joshua Miller <joshsmiller1022@gmail.com>
2021-02-04 22:01:06 -0700

    Header Complete

--- responsive-design-clone/src/Components/Header.js
+++ responsive-design-clone/src/Components/Header.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Header() {
         return <header>
-        
+            <div>
+                <p className="welcome">Welcome To Our Studio!</p>
+                <p className="meet">It's nice to meet you</p>
+            </div>
+            <button id="main-btn">TELL ME MORE</button>
         </header>
 }
 
